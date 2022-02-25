b8f98f21918d078b1e589639344d5d26ffe35c9d
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2022-02-25 14:47:34 -0800

    Remove crufty directories

--- x-ExtractSourceFiles/getAllFileNames.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-case $# in
-1) srcFile=$1;;
-*) echo "Specify the name of the XML file to be parsed."; exit;;
-esac
-
-egrep -e "<(A f)|(F n)=" ${srcFile} | cut -d"\"" -f2 | sort | uniq
