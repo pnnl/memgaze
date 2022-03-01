d01916ff3e1920518b08ddac28b3734868635c4d
Kilic, Ozgur O <ozgur.kilic@pnnl.gov>
2022-03-01 09:28:47 -0800

    cleanup

--- mem-trace/runMulti.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./runGap.sh
-./runMiniVite.sh
--- mem-trace/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-WORD=$(objdump -h /home/kili337/Projects/IPPD/gitlab/palm/intelPT_FP/Experiments/UbenchSet/ubench_set_PTW | grep .dyninstInst)
-echo $WORD
-for w in $WORD
-do
-  echo $w
-done
-stringarray=($WORD)
-echo ${stringarray[5]}
