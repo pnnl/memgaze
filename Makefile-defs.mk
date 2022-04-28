7fd21f8794e45eb7c47ba4fea12f78a2aa1f37ce
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2022-04-28 06:59:46 -0700

    Cleanup

--- Makefile-defs.mk
+++ Makefile-defs.mk
@@ -33,10 +33,6 @@ PREFIX_LIBEXEC = $(PREFIX)/libexec
 
 #-----------------------------------------------------------
 
-# FIXME:
-# xlib_config
-# perf_config
-
 MG_XLIB_ROOT ?= $(MG_XLIB)/lib
 
 DYNINST_ROOT = $(MG_XLIB_ROOT)/dyninst-12.0.1
